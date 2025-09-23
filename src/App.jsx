4f4678318c2d845f09ddaee0ba389b182ed1fae6
Badsha012 <badshasheikh361@gmail.com>
2025-09-23 10:31:28 +0600

    ten commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './Components/Navbar/Navbar'
 import SelectedPlayers from './Components/SelectedPlayers/SelectedPlayers'
 import { Suspense, useState } from 'react'
 import bannerImg from "./assets/bg-shadow.png"
+import footerImg from "./assets/logo-footer.png"
 
   import { ToastContainer } from 'react-toastify';
 
@@ -33,7 +34,7 @@ function App() {
     <>
     <Navbar availableBalace={availableBalace}></Navbar>
         <div
-      className="max-w-[1200px] mx-auto flex flex-col items-center justify-center text-center text-white"
+      className="max-w-[1200px] mx-auto flex flex-col items-center justify-center text-center "
       style={{
           backgroundImage: `url(${bannerImg})`,
          backgroundSize: "cover",
@@ -74,6 +75,54 @@ function App() {
 
     
  <ToastContainer></ToastContainer>
+
+
+ <div className='max-w-[1200px] mx-auto border-2 border-red-600 bg-[#06091a] items-center text-center text-white'>
+
+
+ <div>
+      <img src={footerImg} alt="" className='' srcset="" />
+
+ </div>
+<div className=' grid grid-cols-3 '>
+  <div className=''>
+    <h1>About</h1>
+    <p className='space-y-3'>We are a passionate team dedicated to providing the best services to our customers.</p>
+  </div>
+ <div className=''>
+     <h3 className="text-white font-semibold mb-3">Quick Links</h3>
+          <ul className="space-y-2">
+            <li><a href="#" className="hover:text-white">Home</a></li>
+            <li><a href="#" className="hover:text-white">Services</a></li>
+            <li><a href="#" className="hover:text-white">About</a></li>
+            <li><a href="#" className="hover:text-white">Contact</a></li>
+          </ul>
+ </div>
+ <div className=''>
+     <h3 className="text-white font-semibold mb-3">Subscribe</h3>
+          <p className="text-gray-400 mb-3">
+            Subscribe to our newsletter for the latest updates.
+          </p>
+          <form className="flex">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="flex-1 px-4 py-2 rounded-l-md border focus:outline-none"
+            />
+            <button
+              type="submit"
+              className="px-5 py-2 rounded-r-md font-medium bg-gradient-to-r from-pink-400 to-yellow-400 text-black hover:opacity-90"
+            >
+              Subscribe
+            </button>
+            </form>
+ </div>
+</div>
+
+<div className='items-center  py-10'>
+  <p>@2024 Your Company All Rights Reserved.</p>
+</div>
+ </div>
      
      
     </>
